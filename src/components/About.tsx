3b46cd19d854374590d12abc861c6e2857790715
aliz1369 <alirezazargarian@gmail.com>
2024-11-04 03:14:41 +0300

    .

---
+++ src/components/About.tsx
@@ -0,0 +1,84 @@
+"use client";
+import TabButton from "@/components/TabButton";
+import Image from "next/image";
+import React, { useState, useTransition } from "react";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>React Native</li>
+        <li>MongoDB</li>
+        <li>NestJs</li>
+        <li>Next Js</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Computer Engineering</li>
+      </ul>
+    ),
+  },
+];
+const About = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: string) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image alt="s" src="/images/hacker.gif" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a skilled full-stack developer with 9 years in tech,
+            transitioning from Android to React and React Native. Proficient in
+            Java, Kotlin, JavaScript, TypeScript, Redux, Node.js, Express,
+            NestJs, MongoDB,PostgreSQL and Git, i builds scalable web and mobile
+            solutions.I am a quick learner and I am always looking to expand my
+            knowledge and skill set. I am a team player and I am excited to work
+            with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#afafb1] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>Alireza...</span>
+        <p className="text-[#ADB7BE]">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
